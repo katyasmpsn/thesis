56e6871007a8414792eecd0ee80485060f87130f
katyasmpsn <katya.simpson@berkeley.edu>
2022-01-11 14:24:46 -0500

    add PCA pre-clustering

--- clustering.py
+++ clustering.py
@@ -4,8 +4,13 @@ from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 
 # reading in embeddings
-df = pd.read_csv("embeddings.csv")
+df = pd.read_csv("embeddings.csv", index_col=False)
+# not sure why this column exists
+df = df.drop(columns=["Unnamed: 0"])
+word_type_list = df.columns.tolist()
 
+# transpose for PCA
+df = df.T
 """
 Step 1:
 Initial PCA reduction. Sia et al found that KM allows for dimension reduction of up to 80%.
@@ -24,8 +29,26 @@ Setting N = 20 for some initial testing
 """
 
 # based on https://www.kaggle.com/minc33/visualizing-high-dimensional-clusters
-kmeans = KMeans(n_clusters=3, random_state=0)
-kmeans.fit(X)
+
+X["word_type"] = word_type_list
+weights = pd.read_csv("vocab_counts.csv")
+weights.columns = ["word_type", "tf"]
+X = pd.merge(X, weights, how="left", on="word_type")
+
+word_type = X["word_type"].to_list()
+weights = X["tf"].to_list()
+X = X.drop(columns = ["word_type", "tf"])
+
+kmeans = KMeans(n_clusters=20, random_state=0)
+kmeans.fit(X, sample_weight=weights)
 clusters = kmeans.predict(X)
+
 X["Cluster"] = clusters
+X["Word_Type"] = word_type
+X["Weights"] = weights
+
+outfile = "clusters.csv"
+of = open(outfile, 'w')
+X[["Cluster","Word_Type","Weights"]].to_csv(of)
+of.close()
 
