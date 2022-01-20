2595e9904bd962e0f674ee9cd3d940a8e650bc9f
katyasmpsn <katya.simpson@berkeley.edu>
2022-01-20 17:40:39 -0500

    black'd file

--- src/generate_embeddings.py
+++ src/generate_embeddings.py
@@ -21,6 +21,7 @@ logging.set_verbosity_error()
 clean_data = "data/cleaned_data.csv"
 outfile = "results/debug_embeddings.csv"
 
+
 def getTokenEmbeddings(t1, model, t):
     """
     INPUT: full text
@@ -44,22 +45,28 @@ def getTokenEmbeddings(t1, model, t):
     words_ids = tokens.word_ids()
 
     encoded_input = t(t1, return_tensors="pt")
-    output = model(**encoded_input) # returns a tensor of torch.Size([1, N, 768]) where N is the length of
+    output = model(
+        **encoded_input
+    )  # returns a tensor of torch.Size([1, N, 768]) where N is the length of
     # the tokenized array with subwords and CLS tokens. In the lorax example, N=10
 
     # Average subword representations
     # Generate dummies for words_ids, multiply by the tensor with the last hidden state
-    wi_d = pd.get_dummies(pd.Series(words_ids)).T # this has the shape (# full tokens) x (# subword tokens)
+    wi_d = pd.get_dummies(
+        pd.Series(words_ids)
+    ).T  # this has the shape (# full tokens) x (# subword tokens)
     # in the lorax example: 6 x 10
-    wi_d = wi_d / np.sum(wi_d, axis=1)[:, None] # each row should sum to 1 for an average
+    wi_d = (
+        wi_d / np.sum(wi_d, axis=1)[:, None]
+    )  # each row should sum to 1 for an average
 
     # matrix multiplication of wi_d and a matrix with dims [(# subword tokens) x 768]
     reduced_states = torch.matmul(
-        torch.from_numpy(wi_d.values.astype("float32")), torch.squeeze(output["last_hidden_state"])
+        torch.from_numpy(wi_d.values.astype("float32")),
+        torch.squeeze(output["last_hidden_state"]),
     )
 
     # reduced states has dims [(# FULL tokens) x 768]
-
 
     words = t1.split()
     res = {words[i]: reduced_states[i] for i in range(len(words))}
@@ -69,6 +76,7 @@ def getTokenEmbeddings(t1, model, t):
 
     return res
 
+
 def process_embeddings(lst):
     """
     INPUT: List of dicts of words and embeddings
@@ -76,13 +84,18 @@ def process_embeddings(lst):
     """
 
     word_lst = [list(x.keys()) for x in lst]
-    #collapse list of lists into a 1-D list
+    # collapse list of lists into a 1-D list
     word_lst = [y for x in word_lst for y in x]
 
     # List of numpy arrays
-    np_arr_list = np.vstack(tuple([torch.stack(tuple(list(x.values())), dim=0).detach().numpy() for x in lst]))
+    np_arr_list = np.vstack(
+        tuple(
+            [torch.stack(tuple(list(x.values())), dim=0).detach().numpy() for x in lst]
+        )
+    )
 
     return word_lst, np_arr_list
+
 
 # read in cleaned data generated from preprocessing.py
 clean_file = open(clean_data)
@@ -99,20 +112,19 @@ df = df[df["noteTextList"].apply(lambda x: len(x) < 512)]
 
 # chunking
 # https://stackoverflow.com/questions/44729727/pandas-slice-large-dataframe-into-chunks
-n = 240  #chunk row size
-list_df = [df[i:i+n] for i in range(0,df.shape[0],n)]
+n = 240  # chunk row size
+list_df = [df[i : i + n] for i in range(0, df.shape[0], n)]
 
 words_list = []
 
-if 'np_embeds' in globals():
+if "np_embeds" in globals():
     # TODO: what is up with np_embeds?
     del np_embeds
 
 # for i in range(len(list_df)):
 # or use smaller range below for local debugging
 for i in range(2):
-    print("chunk {0}/{1}".format
-          (i, len(list_df)))
+    print("chunk {0}/{1}".format(i, len(list_df)))
 
     # Instantiate chunk-level bert model
     model = BertModel.from_pretrained("bert-base-uncased")
@@ -123,7 +135,9 @@ for i in range(2):
         return getTokenEmbeddings(t1, model, t)
 
     # Extract embeddings from the words
-    embeddings_loc = list_df[i]["noteText"].apply(lambda x: getTokenEmbeddingsLoc(x)).to_list()
+    embeddings_loc = (
+        list_df[i]["noteText"].apply(lambda x: getTokenEmbeddingsLoc(x)).to_list()
+    )
 
     # Collapse embeddings into a numpy array
     names_loc, np_embeds_loc = process_embeddings(embeddings_loc)
@@ -146,16 +160,16 @@ for i in range(2):
 print("The length of vocab is {}".format(len(words_list)))
 
 # Put results into a Pandas dataframe at the very end
-embed_varnames = ['dim_' + str(i) for i in range(768)]
+embed_varnames = ["dim_" + str(i) for i in range(768)]
 df_embeds = pd.DataFrame(np_embeds, columns=embed_varnames)
-df_embeds['word'] = words_list
-cols_order = ['word', *embed_varnames]
+df_embeds["word"] = words_list
+cols_order = ["word", *embed_varnames]
 df_embeds = df_embeds[cols_order]
 
 # Generate a word count and a mean of the embeddings
-word_count = df_embeds.groupby(['word'])[embed_varnames[0]].count()
+word_count = df_embeds.groupby(["word"])[embed_varnames[0]].count()
 word_count = word_count.sort_values(ascending=False)
-df_embeds = df_embeds.groupby(['word'])[embed_varnames].mean()
+df_embeds = df_embeds.groupby(["word"])[embed_varnames].mean()
 
 ## Vocabulary list
 print("The length of the deduplicated vocab is {}".format(df_embeds.shape[0]))
