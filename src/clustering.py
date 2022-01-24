b7f086f2af978367a466ad9838ff4ba30ca93249
katyasmpsn <katya.simpson@berkeley.edu>
2022-01-24 13:30:05 -0500

    Re-wrote clustering code to take in new format of embeddings

--- src/clustering.py
+++ src/clustering.py
@@ -4,34 +4,34 @@ from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 
 # reading in embeddings
-df = pd.read_csv("../results/embeddings.csv", index_col=False)
+df = pd.read_csv("results/embeddings/note_embeddings_jan21.csv", index_col=False)
 # not sure why this column exists
-df = df.drop(columns=["Unnamed: 0"])
-word_type_list = df.columns.tolist()
+# df = df.drop(columns=["Unnamed: 0"])
 
-# transpose for PCA
-df = df.T
+word_type_list = df.word.tolist()
+
 """
 Step 1:
 Initial PCA reduction. Sia et al found that KM allows for dimension reduction of up to 80%.
-In Sia et al, this dimension reduction was primarily to reduce clustering complexity. 
-Not sure if the same logic applies for this exercise. 
+In Sia et al, this dimension reduction was primarily to reduce clustering complexity.
+Not sure if the same logic applies for this exercise.
 This should be tested as a hyperparameter; but we can start by going from 768 -> 100
 """
 
+
 pca_100d = PCA(n_components=100)
-X = pd.DataFrame(pca_100d.fit_transform(df))
+X = pd.DataFrame(pca_100d.fit_transform(df.drop(columns=["word"])))
 
 """
 Step 2:
-KMeans with N topics (centroids). Again, topics might be used a hyperparameter 
+KMeans with N topics (centroids). Again, topics might be used a hyperparameter
 Setting N = 20 for some initial testing
 """
 
 # based on https://www.kaggle.com/minc33/visualizing-high-dimensional-clusters
 
 X["word_type"] = word_type_list
-weights = pd.read_csv("../results/vocab_counts.csv")
+weights = pd.read_csv("results/note_vocab_counts.csv")
 weights.columns = ["word_type", "tf"]
 X = pd.merge(X, weights, how="left", on="word_type")
 
@@ -47,7 +47,7 @@ X["Cluster"] = clusters
 X["Word_Type"] = word_type
 X["Weights"] = weights
 
-outfile = "../results/clusters_pca_100.csv"
+outfile = "results/clusters_notes_100_jan24.csv"
 of = open(outfile, 'w')
 X[["Cluster","Word_Type","Weights"]].to_csv(of)
 of.close()
